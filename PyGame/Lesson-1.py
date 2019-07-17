4aee6c80f523571f77ea4000d99319e926feeb21
Vladislav Semenov <affliction001@mail.ru>
2019-07-17 18:22:49 +0300

    install pygame

---
+++ PyGame/Lesson-1.py
@@ -0,0 +1,14 @@
+import pygame
+
+# NEVER START THE PROGRAM!!!
+
+# pygame.init()
+# screen = pygame.display.set_mode((640, 480))
+#
+# while True:
+#     pygame.display.flip()
+
+import pygame.examples.stars
+pygame.examples.stars.main()
+
+
